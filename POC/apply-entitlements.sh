6d920018735d39fd7dde6b79c1c6cc0487363b10
Eric Sonchaiwanich <eric@galacticfog.com>
2018-11-08 23:58:12 -0500

    Fix apply entitlements

--- POC/apply-entitlements.sh
+++ POC/apply-entitlements.sh
@@ -1,4 +1,6 @@
 
-fog admin apply-entitlements --group devs -f entitlements/all-org.yaml --path /poc
-fog admin apply-entitlements --group devs -f entitlements/all-workspace.yaml --path /poc/sample
-fog admin apply-entitlements --group devs -f entitlements/all-environment.yaml --path /poc/sample/dev
+set -e
+
+fog admin apply-entitlements --group devs -f entitlements/readonly-org.yaml --path /poc
+fog admin apply-entitlements --group devs -f entitlements/readonly-workspace.yaml --path /poc/sample
+fog admin apply-entitlements --group devs -f entitlements/developer-dev-environment.yaml --path /poc/sample/dev
--- POC/get-meta-info.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kubectl describe deploy --all-namespaces | grep GESTALT_SECURITY_KEY
-kubectl describe deploy --all-namespaces | grep GESTALT_SECURITY_SECRET

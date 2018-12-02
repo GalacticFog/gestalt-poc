7a8ff009d526fabb78be094be6fc6dd307b08a3f
Eric Sonchaiwanich <eric@galacticfog.com>
2018-12-02 14:51:09 -0500

    Training updates - add policy rules to student environment

--- Training/apply.sh
+++ Training/apply.sh
@@ -2,29 +2,19 @@
 
 set -e
 
+echo "Creating Training Org"
+fog apply -f setup/training-org.yaml
+
 echo "Creating Demo Workspace"
 fog apply -d setup/demo_workspace
 
-echo "Creating lambda_demo environment for Student 00"
-fog apply -f lambda_demo/env.yaml  --context /training/demo
-fog apply -d lambda_demo  --context /training/demo/lambda_demo --params api=demo-lambda-demo
+org=training
 
-echo "Creating lambda_chaingin environment for Student 00"
-fog apply -f lambda_chaining/env.yaml  --context /training/demo
-fog apply -d lambda_chaining  --context /training/demo/lambda_chaining --params api=demo-lambda-chaining
+echo "Creating lambda_demo environment"
+fog apply -f lambda_demo/env.yaml  --context /$org/demo
+fog apply -d lambda_demo  --context /$org/demo/lambda_demo --params api=demo-lambda-demo
 
-students=$@
-
-for i in $students ; do
-    echo "Creating workspace for Student $i"
-    fog apply -d setup/student_workspace --params student_id=$i
-
-    echo "Creating lambda_demo environment for Student $i"
-    fog apply -f lambda_demo/env.yaml  --context /training/student-$i
-    fog apply -d lambda_demo  --context /training/student-$i/lambda_demo --params api=student-$i-lambda-demo
-
-    echo "Creating lambda_chaingin environment for Student $i"
-    fog apply -f lambda_chaining/env.yaml  --context /training/student-$i
-    fog apply -d lambda_chaining  --context /training/student-$i/lambda_chaining --params api=student-$i-lambda-chaining
-done
+echo "Creating lambda_chaining environment"
+fog apply -f lambda_chaining/env.yaml  --context /$org/demo
+fog apply -d lambda_chaining  --context /$org/demo/lambda_chaining --params api=demo-lambda-chaining
 

3138b42431c5bb27a0fc53a5b4247067efe131fe
Eric Sonchaiwanich <eric@galacticfog.com>
2018-12-01 20:52:01 -0500

    Training update

--- Training/apply.sh
+++ Training/apply.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-students="01"
+set -e
 
-# for i in $students ; do
-#     fog apply -d setup/workspace_template --params student_id=$i
-# done
+echo "Creating Demo Workspace"
+fog apply -d setup/demo_workspace
+
+echo "Creating lambda_demo environment for Student 00"
+fog apply -f lambda_demo/env.yaml  --context /training/demo
+fog apply -d lambda_demo  --context /training/demo/lambda_demo --params api=demo-lambda-demo
+
+echo "Creating lambda_chaingin environment for Student 00"
+fog apply -f lambda_chaining/env.yaml  --context /training/demo
+fog apply -d lambda_chaining  --context /training/demo/lambda_chaining --params api=demo-lambda-chaining
+
+students=$@
 
 for i in $students ; do
-    fog apply -f lambda_demo/env.yaml  --context /training/student-$i --params student_id=$i
-    fog apply -d lambda_demo  --context /training/student-$i/lambda_demo --params api=student-$i-lambda-demo student_id=$i
-done
+    echo "Creating workspace for Student $i"
+    fog apply -d setup/student_workspace --params student_id=$i
 
-# for i in $students ; do
-#     fog apply -d lambda_chaining  --context /training/student-$i/local --params api=student-$i-local env=lambda_chaining
-# done
+    echo "Creating lambda_demo environment for Student $i"
+    fog apply -f lambda_demo/env.yaml  --context /training/student-$i
+    fog apply -d lambda_demo  --context /training/student-$i/lambda_demo --params api=student-$i-lambda-demo
+
+    echo "Creating lambda_chaingin environment for Student $i"
+    fog apply -f lambda_chaining/env.yaml  --context /training/student-$i
+    fog apply -d lambda_chaining  --context /training/student-$i/lambda_chaining --params api=student-$i-lambda-chaining
+done
 

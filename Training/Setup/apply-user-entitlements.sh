48934b664c5d2430b439986caab7e2625c1d7d13
Eric Sonchaiwanich <eric@galacticfog.com>
2018-12-03 22:25:42 -0500

    Remove add-only

--- Training/Setup/apply-user-entitlements.sh
+++ Training/Setup/apply-user-entitlements.sh
@@ -5,14 +5,14 @@ set -e
 user=$1
 
 # Training org / workspace / environments
-fog admin apply-entitlements --user $user -f entitlements/readonly-org.yaml /root --add-only
-fog admin apply-entitlements --user $user -f entitlements/readonly-org.yaml /training --add-only
-fog admin apply-entitlements --user $user -f entitlements/readonly-workspace.yaml /training/demo --add-only
-fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /training/demo/lambda_chaining --add-only
-fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /training/demo/lambda_demo --add-only
-fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /training/demo/async_lambdas --add-only
-fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /training/demo/periodic_lambdas --add-only
+fog admin apply-entitlements --user $user -f entitlements/readonly-org.yaml /root
+fog admin apply-entitlements --user $user -f entitlements/readonly-org.yaml /training
+fog admin apply-entitlements --user $user -f entitlements/readonly-workspace.yaml /training/demo
+fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /training/demo/lambda_chaining
+fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /training/demo/lambda_demo
+fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /training/demo/async_lambdas
+fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /training/demo/periodic_lambdas
 
 # User
-fog admin apply-entitlements --user $user -f entitlements/readonly-workspace.yaml /training/${user,,} --add-only
-fog admin apply-entitlements --user $user -f entitlements/developer-dev-environment.yaml /training/${user,,}/local --add-only
+fog admin apply-entitlements --user $user -f entitlements/readonly-workspace.yaml /training/${user,,}
+fog admin apply-entitlements --user $user -f entitlements/developer-dev-environment.yaml /training/${user,,}/local

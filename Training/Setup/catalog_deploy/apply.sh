98895ccbcdc8365fb5c1f05e176f6f881eecf114
Eric Sonchaiwanich <eric@galacticfog.com>
2019-01-10 09:41:30 -0500

    WIP: Catalog deploy lambda

---
+++ Training/Setup/catalog_deploy/apply.sh
@@ -0,0 +1,3 @@
+fog context set /root/gestalt-system-workspace/gestalt-system-environment
+
+fog apply -d . --config ../config.yaml --params api=catalog-deploy
--- Training/Setup/catalog_deploy/src/catalog-deploy-bundle.js
+++ Training/Setup/catalog_deploy/src/catalog-deploy-bundle.js
@@ -1,27 +1,33 @@
 const { gestalt, actions } = require('gestalt-fog-sdk');
 const request = require('request-promise-native');
 
-module.exports.handler = async (event, context, callback) => {
+module.exports.handler = async (event, ctx, callback) => {
 
     try {
+        console.log('Event: ' + event);
+        console.log('Context: ' + ctx);
 
-        // console.log('Event: ' + event);
-        // console.log('Context: ' + context);
+        event = parse(event);
+        ctx = parse(ctx);
 
-        event = (event && event.length > 0) ? JSON.parse(event) : {};
-        context = (context && context.length > 0) ? JSON.parse(context) : {};
+        await initialize();
 
-        //
-        // TODO - use Gestalt SDK to deploy the bundle
-        //
+        const context = getContext(event);
+
+        const data = JSON.parse(event.payload);
+
+        console.log('Applying resources...');
+
+        const { succeeded, failed } = await actions.applyResources(context, data.resources, data.options, data.config);
+
+        console.log('Showing results...');
 
         const body = {
-            event: event,
-            context: context
+            succeeded, failed
         }
 
         const response = {
-            statusCode: 201,
+            statusCode: 200,
             headers: {},
             body: JSON.stringify(body, null, 2)
         }
@@ -36,3 +42,38 @@ module.exports.handler = async (event, context, callback) => {
         callback(null, response);
     }
 };
+
+function parse(o) {
+    return (o && o.length > 0) ? JSON.parse(o) : {};
+}
+
+function getContext(event) {
+    // const contextPath = ctx.context.contextMeta.fqon + '/' + ctx.context.workspace.name + '/' + ctx.context.environment.name;
+    return {
+        org: {
+            fqon: event.context.contextMeta.fqon
+        },
+        workspace: {
+            id: event.context.contextMeta.workspaceId
+        },
+        environment: {
+            id: event.context.contextMeta.environmentId
+        },
+    };
+}
+
+
+async function initialize() {
+    // Configuration params
+    gestalt.configure({
+        gestalt_url: 'required-but-not-used',
+        meta_url: 'http://gestalt-meta.gestalt-system.svc.cluster.local:10131',
+        security_url: 'http://gestalt-security.gestalt-system.svc.cluster.local:9455',
+    })
+
+    // Do login
+    const res = await gestalt.login({
+        username: 'admin',
+        password: 'gestaltpoc!'
+    });
+}

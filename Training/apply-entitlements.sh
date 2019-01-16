aa74077b9c9fb773bccf59e217563ace479699f1
Eric Sonchaiwanich <eric@galacticfog.com>
2019-01-16 00:37:23 -0500

    timing for entitlements

--- Training/apply-entitlements.sh
+++ Training/apply-entitlements.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-for user in `cat ../../all-users.txt`; do 
+time for user in `cat ../../all-users.txt`; do 
     echo "Applying entitlements for $user ..."
 
     # Training org / workspace / environments
     # fog admin apply-entitlements --user $user -f entitlements/readonly-org.yaml /root
-    fog admin apply-entitlements --user $user -f ../entitlements/readonly-org.yaml /training
+    time fog admin apply-entitlements --user $user -f ../entitlements/readonly-org.yaml /training
 
     # User
     # fog admin apply-entitlements --user $user -f ../entitlements/readonly-workspace.yaml /training/${user,,}
-    fog admin apply-entitlements --user $user -f ../entitlements/developer-dev-environment.yaml /training/${user,,}/local
+    time fog admin apply-entitlements --user $user -f ../entitlements/developer-dev-environment.yaml /training/${user,,}/local
 
 done

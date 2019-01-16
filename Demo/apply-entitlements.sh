bdf234ea1eed21560a02fe8f4672c4e8551882ee
Eric Sonchaiwanich <eric@galacticfog.com>
2019-01-16 00:09:06 -0500

    Entitlements

--- Demo/apply-entitlements.sh
+++ Demo/apply-entitlements.sh
@@ -3,14 +3,14 @@
 # for user in `cat ../../normal-users.txt`; do 
 #     echo "Applying entitlements for $user ..."
 
-#     fog admin apply-entitlements --user $user -f entitlements/readonly-org.yaml /root
-#     fog admin apply-entitlements --user $user -f entitlements/readonly-workspace.yaml /root/demo
-#     fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /root/demo/lambda_chaining
-#     fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /root/demo/lambda_demo
-#     fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /root/demo/async_lambdas
-#     fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /root/demo/periodic_lambdas
-#     fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /root/demo/vm_provision
-#     fog admin apply-entitlements --user $user -f entitlements/readonly-environment.yaml /root/demo/url_redirect
+#     fog admin apply-entitlements --user $user -f ../entitlements/readonly-org.yaml /root
+#     fog admin apply-entitlements --user $user -f ../entitlements/readonly-workspace.yaml /root/demo
+#     fog admin apply-entitlements --user $user -f ../entitlements/readonly-environment.yaml /root/demo/lambda_chaining
+#     fog admin apply-entitlements --user $user -f ../entitlements/readonly-environment.yaml /root/demo/lambda_demo
+#     fog admin apply-entitlements --user $user -f ../entitlements/readonly-environment.yaml /root/demo/async_lambdas
+#     fog admin apply-entitlements --user $user -f ../entitlements/readonly-environment.yaml /root/demo/periodic_lambdas
+#     fog admin apply-entitlements --user $user -f ../entitlements/readonly-environment.yaml /root/demo/vm_provision
+#     fog admin apply-entitlements --user $user -f ../entitlements/readonly-environment.yaml /root/demo/url_redirect
 
 # done
 
@@ -18,13 +18,13 @@
 for user in `cat ../../core-users.txt`; do 
     echo "Applying entitlements for $user ..."
 
-    fog admin apply-entitlements --user $user -f entitlements/readonly-org.yaml /root
-    fog admin apply-entitlements --user $user -f entitlements/admin-workspace.yaml /root/demo
-    fog admin apply-entitlements --user $user -f entitlements/admin-environment.yaml /root/demo/lambda_chaining
-    fog admin apply-entitlements --user $user -f entitlements/admin-environment.yaml /root/demo/lambda_demo
-    fog admin apply-entitlements --user $user -f entitlements/admin-environment.yaml /root/demo/async_lambdas
-    fog admin apply-entitlements --user $user -f entitlements/admin-environment.yaml /root/demo/periodic_lambdas
-    fog admin apply-entitlements --user $user -f entitlements/admin-environment.yaml /root/demo/vm_provision
-    fog admin apply-entitlements --user $user -f entitlements/admin-environment.yaml /root/demo/url_redirect
+    fog admin apply-entitlements --user $user -f ../entitlements/readonly-org.yaml /root
+    fog admin apply-entitlements --user $user -f ../entitlements/admin-workspace.yaml /root/demo
+    fog admin apply-entitlements --user $user -f ../entitlements/admin-environment.yaml /root/demo/lambda_chaining
+    fog admin apply-entitlements --user $user -f ../entitlements/admin-environment.yaml /root/demo/lambda_demo
+    fog admin apply-entitlements --user $user -f ../entitlements/admin-environment.yaml /root/demo/async_lambdas
+    fog admin apply-entitlements --user $user -f ../entitlements/admin-environment.yaml /root/demo/periodic_lambdas
+    fog admin apply-entitlements --user $user -f ../entitlements/admin-environment.yaml /root/demo/vm_provision
+    fog admin apply-entitlements --user $user -f ../entitlements/admin-environment.yaml /root/demo/url_redirect
 
 done
